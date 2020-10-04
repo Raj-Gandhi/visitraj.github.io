8deaed24c23f025fddd46203f263206147660193
raj <grajharishkumar02@gmail.com>
2020-10-04 20:32:21 +0530

    add project

---
+++ precache-manifest.9af9e4f28f5aaf2067d248c99a19fa33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4547e84be5b1fd2eed76db30dc19eac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69dc9f1a730da825236f",
+    "url": "/static/css/3.1123fc4f.chunk.css"
+  },
+  {
+    "revision": "2156482a6e670649bd8c",
+    "url": "/static/css/main.cf920443.chunk.css"
+  },
+  {
+    "revision": "fa9743645a782fb93bbb",
+    "url": "/static/js/2.2a2167a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a2167a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69dc9f1a730da825236f",
+    "url": "/static/js/3.732d37c0.chunk.js"
+  },
+  {
+    "revision": "2156482a6e670649bd8c",
+    "url": "/static/js/main.d3842245.chunk.js"
+  },
+  {
+    "revision": "da928cb90ff054f9c5ca",
+    "url": "/static/js/runtime-main.afc22451.js"
+  },
+  {
+    "revision": "25d5fb55628b86b8a752525e18da3342",
+    "url": "/static/media/handFont.25d5fb55.ttf"
+  },
+  {
+    "revision": "6e5dfd5d33820344a5e284fe5bc66afe",
+    "url": "/static/media/handFont2.6e5dfd5d.ttf"
+  },
+  {
+    "revision": "8f3d822a262413bd0482463664a6f67a",
+    "url": "/static/media/handFont3.8f3d822a.otf"
+  }
+]);
